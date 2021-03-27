66f00f7e5ebe1e7938bc4167deb030ef6a871010
davidgq10 <davidgq10@hotmail.com>
2021-03-27 20:27:03 +0000

    Entrega de proyecto

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,11 +5,12 @@ export function Home() {
 	//Declaración de Hooks del componente
 	const [tasklist, setTaskList] = useState([]);
 	const [task, setTask] = useState("");
+	const [hoverli, setHoverli] = useState(false);
 
 	//Evento que se genera al presionar una tecla del input newtask
 	const handleOnKeyPress = e => {
 		//Verifica si la tecla presionada es enter, true agrega elemento a lista de tareas
-		if (e.key === "Enter") {
+		if (e.key === "Enter" && task !== "") {
 			e.preventDefault();
 			//define la newTask que será añadida al array
 			const newTask = {
@@ -20,40 +21,47 @@ export function Home() {
 			setTaskList([...tasklist].concat(newTask));
 			//reinicia el valor de task
 			setTask("");
+		} else if (e.key === "Enter" && task == "") {
+			alert("Upps, you must enter a task");
 		}
 	};
 
 	const generarLista = () => {
-        //recorre el objeto y genera los elementos de la lista
+		//recorre el objeto y genera los elementos de la lista
 		return tasklist.map(task => (
-			<li key={task.id} className="list-group-item">
-				<p className="d-inline-block text-secondary ml-5 align-middle">
+			<li
+				key={task.id}
+				className="list-group-item"
+				onMouseEnter={() => setHoverli(task.id)}>
+				<p className="d-inline-block text-secondary ml-4 fs-3 align-middle rounded-0">
 					{task.description}
 				</p>
-				<button
-					type="button"
-					className="btn btn-light float-right"
-					onClick={() => deleteTask(task.id)}>
-					<i className="fas fa-times"></i>
-				</button>
+				{task.id == hoverli ? (
+					<button
+						type="button"
+						className="btn btn-light float-right"
+						onClick={() => deleteTask(task.id)}>
+						<i className="fas fa-times"></i>
+					</button>
+				) : null}
 			</li>
 		));
 	};
 
-    // función para eliminar task al dar click al button
-	function deleteTask(id) {
-		let updateTaskList = [...tasklist].filter(task => task.id !== id);
+	// función para eliminar task al dar click al button
+	const deleteTask = id => {
+		const updateTaskList = [...tasklist].filter(task => task.id !== id);
 		setTaskList(updateTaskList);
-	}
+	};
 
 	//genera el componente
 	return (
 		<div className="container">
-			<h1 className="text-muted text-center display-4">To-Do</h1>
+			<h1 className="text-muted text-center display-4">Todos</h1>
 			<input
 				type="text"
 				placeholder="Type a new task"
-				className="form-control mb-2 text-secondary"
+				className="form-control text-secondary rounded-0"
 				value={task}
 				onChange={e => setTask(e.target.value)}
 				onKeyPress={e => handleOnKeyPress(e)}
@@ -63,7 +71,9 @@ export function Home() {
 				<div>
 					<label htmlFor="list-group-item">
 						<p className="text-secondary ml-5 mt-2">
-							{tasklist.length} item left
+							{tasklist.length == 0
+								? "No tasks, add a task"
+								: tasklist.length + " item left"}
 						</p>
 					</label>
 				</div>
